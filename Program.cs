903841be154083c36229b08ce1f9faca3242130d
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-04-03 10:28:23 -0500

    complete lesson about parameter modifier params

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Student {
 
+    public void DisplaySubjects(params string[] subjects) {
+
+        for(int i = 0; i < subjects.Length; i++) {
+
+            Console.WriteLine(subjects[i]);
+        }
+        
+    }
+
+    class Program
+    {
+
+        static void Main()
+        {
+
+            //create object of student class
+            Student s = new Student();
+
+            //access display subjects method
+            s.DisplaySubjects("Theory of Computation", "Computer Networks", "Discrete Mathemetics", "Digital System Design", "Basics of C");
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
